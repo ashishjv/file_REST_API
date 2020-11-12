0fee3802156dc989042041167f706136678c6ef8
Ashish Joseph Vattoth <12287103+ashishjv@users.noreply.github.com>
2020-11-11 18:47:04 -0600

    First commit

---
+++ src/main/java/com/example/file_REST_API/FileRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.file_REST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileRestApiApplication.class, args);
+	}
+
+}
