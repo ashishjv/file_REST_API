98b621a6393101f7a2939e4912bd3d2fa017c1ef
Ashish Joseph Vattoth <12287103+ashishjv@users.noreply.github.com>
2020-11-11 20:39:54 -0600

    API added

--- src/main/java/com/example/file_REST_API/FileRestApiApplication.java
+++ src/main/java/com/example/file_REST_API/FileRestApiApplication.java
@@ -2,7 +2,9 @@ package com.example.file_REST_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class FileRestApiApplication {
 
